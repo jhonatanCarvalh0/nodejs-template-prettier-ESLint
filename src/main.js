25823640fba258892368413dd7ed3a2f79e55cb2
jhonatanCarvalh0 <jhonatan.silva.4198@gmail.com>
2022-09-30 14:14:19 -0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 const numero = '2';
-console.info(`numero: ${numero}`)
+console.info(`numero: ${numero}`);
 
 if (numero === 2 && numero === 2) {
-        console.info('data 4')
-                    console.info(`data z${numero}`);
+            console.info('data 4');
+    console.info(`data z${numero}`);
 }
