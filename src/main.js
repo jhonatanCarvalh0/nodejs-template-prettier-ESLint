35803803199a2e1a6172d11b546170f01fba3b19
jhonatanCarvalh0 <jhonatan.silva.4198@gmail.com>
2022-09-30 11:26:51 -0300

    Configuração do Prettier

--- src/main.js
+++ src/main.js
@@ -1,2 +1,12 @@
 const numero = 2;
-console.info("numero: " + numero);
+console.info('numero: ' + numero);
+
+if (
+    variavelComnomeGrande == 2
+    && variavelComnomeGrande == 2
+    && variavelComnomeGrande == 2
+    && variavelComnomeGrande == 2
+    && variavelComnomeGrande == 2
+) {
+    console.log('numero é igual 2');
+}
