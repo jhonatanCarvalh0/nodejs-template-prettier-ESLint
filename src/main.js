abd93e81885282e7d6a58c219cdcd5541f78f862
jhonatanCarvalh0 <jhonatan.silva.4198@gmail.com>
2022-09-30 15:23:10 -0400

    test 2 - github desktop - husky/eslint/prettier quick

--- src/main.js
+++ src/main.js
@@ -2,6 +2,6 @@ const numero = '2';
 console.info(`numero: ${numero}`);
 
 if (numero === 2 && numero === 2) {
-    console.info('data 4');
-    console.info(`data z${numero}`);
+            console.info('data 4');
+                                     console.info(`data z${numero}`);
 }
