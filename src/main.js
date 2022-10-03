44f42039d3c6540d6c13755fb3fb98f3d336001b
jhonatanCarvalh0 <jhonatan.silva.4198@gmail.com>
2022-10-03 16:27:11 -0400

    template ready

--- src/main.js
+++ src/main.js
@@ -2,6 +2,6 @@ const numero = '2';
 console.info(`numero: ${numero}`);
 
 if (numero === 2 && numero === 2) {
-        console.info('data 4');
+    console.info('data 5');
     console.info(`data z${numero}`);
 }
